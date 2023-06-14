kylinfedora/FFmpeg

avcodec/rkmppdec: Add AV1
Note: For new chips like rk3588.

Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

libavcodec/allcodecs.c
@@ -824,6 +824,7 @@ extern const FFCodec ff_libaom_av1_decoder;
 extern const FFCodec ff_av1_decoder;
 extern const FFCodec ff_av1_cuvid_decoder;
 extern const FFCodec ff_av1_qsv_decoder;
+extern const FFCodec ff_av1_rkmpp_decoder;
 extern const FFCodec ff_libopenh264_encoder;
 extern const FFCodec ff_libopenh264_decoder;
 extern const FFCodec ff_h264_amf_encoder;

jjm2473/ffmpeg-rk

libavcodec/allcodecs.c
@@ -41,6 +41,7 @@ extern const AVCodec ff_vp9_rkmpp_decoder;
 extern const AVCodec ff_h263_rkmpp_decoder;
 extern const AVCodec ff_h264_rkmpp_decoder;
 extern const AVCodec ff_hevc_rkmpp_decoder;
+extern const AVCodec ff_av1_rkmpp_decoder;
 
 extern const AVCodec ff_a64multi_encoder;
 extern const AVCodec ff_a64multi5_encoder;